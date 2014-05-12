b037a5cddb086b687a4ba65f40a8c142a5046b32
Rares <rares.mirica@booking.com>
2014-05-12 10:06:55 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="pythonDaemon",
+      version="1.2",
+      description="Python daemon class",
+      url="https://github.com/mrares/python-daemon",
+      author="Rares Mirica",
+      author_email="rares.mirica@booking.com",
+      license="CC BY-SA 3.0",
+      py_modules=['pythonDaemon'])
