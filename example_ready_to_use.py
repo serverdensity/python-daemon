a2d3d9a170a7641e3249f12ba4adb732bd58f22f
grab76 <gest.sebastien@gmail.com>
2017-02-18 14:17:56 -0500

    Add an example ready to use with the start | stop | restart | debug arguments

---
+++ example_ready_to_use.py
@@ -0,0 +1,26 @@
+import sys
+from daemon import Daemon
+
+class pantalaimon(Daemon):
+    def run(self):
+        # Do stuff
+
+try:
+    pineMarten = pantalaimon('/path/to/pid.pid')
+
+    if(str.lower(str(sys.argv[1])) == "stop"):
+	pineMarten.stop()        
+        exit(1)
+
+    if(str.lower(str(sys.argv[1])) == "start"):
+        pineMarten.start()
+
+
+    if(str.lower(str(sys.argv[1])) == "restart"):
+        pineMarten.restart()
+    
+    if(str.lower(str(sys.argv[1])) == "debug"):
+        pineMarten.run()
+
+except Exception as e:
+    print "The argument is missing. The authorized arguments are start , stop , restart , debug"
